8b4384ed4a153b91dea27a8266045c6d1300d137
Scott Numamoto <scott.numamoto@gmail.com>
2016-03-03 17:15:12 -0800

    Convert to Python2

--- robotext.py
+++ robotext.py
@@ -1,3 +1,7 @@
+"""A small world explorer to teach python functions
+
+Online version here: https://repl.it/BsmV
+"""
 #Background varaibles
 WEST = 0
 NORTH = 1
@@ -9,10 +13,13 @@ DIRECTION_STRINGS = ["WEST", "NORTH", "EAST", "SOUTH"]
 LEFT = -1
 RIGHT = 1
 
+
+
 class World:
-	def __init__(self, width=5, height=5):
+	"""A world of text to explore"""
+
+	def __init__(self, width=10, height=10):
 		self.__points = [["." for x in range(width)] for y in range(height)]
-		self.__points[height//2][width//2] = '@'
 		self.__width = width
 		self.__height = height
 		self.__robot = None 
@@ -32,8 +39,7 @@ class World:
 						result += self.__points[y][x]
 				else:
 					result += self.__points[y][x]
-			if y != 0:
-				result += "\n"
+			result += "\n"
 		return result
 
 	def take(self, x,y):
@@ -50,6 +56,11 @@ class World:
 		"""Add a robot into the world to be displayed"""
 		self.__robot = robot
 		self.__robot.add_world(self)
+
+	def set_point(self, x, y, character):
+		"""Set a point on the map of be a certain character"""
+		assert len(character) == 1
+		self.__points[y][x] = character
 
 	def get_height(self):
 		return self.__height
@@ -69,7 +80,7 @@ class Robot:
 		self.__verbose = True
 		self.__inventory = None
 	def __repr__(self):
-		return "Robot, x:%s, y:%s" % (self.__x, self.__y)
+		return "Robot, x:%s, y:%s, inv:%s" % (self.__x, self.__y, self.__inventory)
 	
 	def __str__(self):
 		"""Defines the symbol that robot will be displayed as"""
@@ -99,8 +110,8 @@ class Robot:
 		elif self.__direction == WEST and self.__x - 1 > 0:
 			self.__x -= 1
 		else:
-			print("Invalid Movement")
-		print(self.__world)
+			print "Invalid Movement"
+		print self.__world
 
 	def read(self):
 		"""Inspect what the robot is currently standing above"""
@@ -112,17 +123,20 @@ class Robot:
 	def grab(self):
 		"""Adds the current object to inventory"""
 		if self.__inventory:
-			print("Inventory Full")
+			print "Inventory Full"
 		else:
 			self.__inventory = self.__world.take(self.__x, self.__y)
 
 	def release(self):
 		"""Puts object in the inventory on the ground"""
 		if self.__inventory:
-			self.__world.leave(self.__inventory, self.__x, self.__y)
-			self.__inventory = None
+			if self.read() == '.':
+				self.__world.leave(self.__inventory, self.__x, self.__y)
+				self.__inventory = None
+			else:
+				print "Area blocked"
 		else:
-			print("Inventory empty")
+			print "Inventory empty"
 	def get_inventory(self):
 		return self.__inventory
 
@@ -139,7 +153,23 @@ class Robot:
 	def get_direction(self):
 		return DIRECTION_STRINGS[self.__direction]
 
-if __name__ == "__main__":
+if __name__ == "__main__" or __name__ == "__builtin__":
 	a = World()
+	a.set_point(3,3,'3')
+	a.set_point(4,5,'2')
+	a.set_point(6,1,'4')
+	a.set_point(8,5,'5')
+
+	#Create the hayward world
+	b = World()
+	b.set_point(3,8,'H')
+	b.set_point(8,2,'A')
+	b.set_point(7,8,'Y')
+	b.set_point(1,3,'W')
+	b.set_point(8,6,'A')
+	b.set_point(3,2,'R')
+	b.set_point(6,9,'A')
+	b.set_point(3,5,'D')
+
 	r = Robot()
 	a.set_robot(r)
